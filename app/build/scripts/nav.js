7d5763687638c87201921f1d78f3ad3d04bd13c5
Murod <mambetovmurod80@gmail.com>
2021-03-31 16:32:52 +0500

    car 1.0

---
+++ app/build/scripts/nav.js
@@ -0,0 +1,1 @@
+"use strict";var menu=document.querySelector(".nav__menu"),menuBtn=document.querySelector(".nav__btn");menuBtn.addEventListener("click",function(){menu.classList.toggle("active"),menuBtn.classList.toggle("active"),setTimeout(function(){menu.style.height="100%"},100)});
---
+++ app/src/script/nav.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('.nav__menu')
+let menuBtn = document.querySelector('.nav__btn')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+    setTimeout(() => {
+        menu.style.height = '100%'
+    }, 100);
+})
